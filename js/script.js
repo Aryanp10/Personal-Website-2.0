635e518dac292592ce995bbc4d7f0addb4025637
Aryanp10 <aaryanparajulee@gmail.com>
2021-02-18 13:26:58 -0600

    projects page, organization, modal

--- js/script.js
+++ js/script.js
@@ -14,3 +14,107 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+// PROJECTS LARGE BOX
+
+function showBookingDB() {
+  div = document.getElementById('Booking');
+  if(div.style.display == "block"){
+div.style.display = "none"
+      document.getElementById('Default').style.display = "block"
+
+  }
+  else{
+      document.getElementById('Default').style.display = "none"
+      document.getElementById('College').style.display = "none"
+      document.getElementById('Personal').style.display = "none"
+      document.getElementById('WebS').style.display = "none"
+    div.style.display="block"
+  }
+}
+
+function showCollegeBuddy() {
+  div = document.getElementById('College');
+  if(div.style.display == "block"){
+div.style.display = "none"
+      document.getElementById('Default').style.display = "block"
+
+  }
+  else{
+      document.getElementById('Default').style.display = "none"
+      document.getElementById('Booking').style.display = "none"
+      document.getElementById('Personal').style.display = "none"
+      document.getElementById('WebS').style.display = "none"
+    div.style.display="block"
+  }
+}
+
+function showPersonalWeb() {
+  div = document.getElementById('Personal');
+  if(div.style.display == "block"){
+div.style.display = "none"
+      document.getElementById('Default').style.display = "block"
+
+  }
+  else{
+      document.getElementById('Default').style.display = "none"
+      document.getElementById('College').style.display = "none"
+      document.getElementById('Booking').style.display = "none"
+      document.getElementById('WebS').style.display = "none"
+    div.style.display="block"
+  }
+}
+
+function showWebScraper() {
+  div = document.getElementById('WebS');
+  if(div.style.display == "block"){
+div.style.display = "none"
+      document.getElementById('Default').style.display = "block"
+  }
+  else{
+      document.getElementById('Default').style.display = "none"
+      document.getElementById('College').style.display = "none"
+      document.getElementById('Personal').style.display = "none"
+      document.getElementById('Booking').style.display = "none"
+    div.style.display="block"
+  }
+}
+
+// MODAL
+
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
